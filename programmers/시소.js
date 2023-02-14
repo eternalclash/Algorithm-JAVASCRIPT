ea5e13cc79b364502df374c0c72f449fb2716a45
이수창 <soo@isuchang-ui-MacBookPro.local>
2023-02-14 14:07:48 +0900

    2/14 알고리즘

---
+++ programmers/시소.js
@@ -0,0 +1,34 @@
+class Counter {
+    data = {};
+    add(num) {
+        if (this.data[num] === undefined) {
+            this.data[num] = 1;
+        } else {
+            this.data[num] += 1;  
+        } 
+    }
+    get(num) {
+        if (this.data[num] === undefined) {
+            return 0;
+        } else {
+            return this.data[num];
+        }
+    }
+}
+
+function solution(weights) {
+    const counter = new Counter();
+    weights.sort((a,b)=>a-b);
+    console.log(weights)
+    let result = 0;
+    for (const weight of weights) {
+        result += counter.get(weight);
+        counter.add(weight);
+        counter.add(weight * 4 / 3);
+        counter.add(weight * 1.5);
+        counter.add(weight * 2);
+    }
+    return result;
+}
+
+//맞을 수 있는 합을 생각해 보자 그리고 그것을 더하자
